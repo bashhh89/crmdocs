ef82d38f3b23162aabb1ddea59fa2fc58ca4be28
Khaled Bashir <ahmadbasheerr@gmail.com>
2026-04-19 12:01:20 +0200

    analytics: add Umami tracking snippet for docs.ancsports.net
    Site ID f26fd606-215f-4a41-a237-4e769a59a5ff on abc-umami.izcgmb.easypanel.host.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -9,6 +10,14 @@ const inter = Inter({
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://abc-umami.izcgmb.easypanel.host/script.js"
+          data-website-id="f26fd606-215f-4a41-a237-4e769a59a5ff"
+          strategy="afterInteractive"
+          defer
+        />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
